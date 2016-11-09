ab9993aea8399d6a490a426ddd40b96aba8c8b86
Dillon Baker <baker.dillon.01@gmail.com>
2016-11-09 13:36:03 -0500

    Add code first migrations; Add Book class to database;

---
+++ Library/Library/Models/Book.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Library.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public int Title { get; set; }
+        public string Author { get; set; }
+        public string Publisher { get; set; }
+        public DateTime PublicatonDate { get; set; }
+        public int NumberOfPages { get; set; }
+    }
+}
