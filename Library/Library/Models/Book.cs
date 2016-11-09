02e93011b0aef2a0317b48ee4ce5029a337159ce
Dillon Baker <baker.dillon.01@gmail.com>
2016-11-09 13:49:25 -0500

    Added Author model to database; Updated Book.Author to be of type Author;

--- Library/Library/Models/Book.cs
+++ Library/Library/Models/Book.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,10 +9,19 @@ namespace Library.Models
     public class Book
     {
         public int Id { get; set; }
-        public int Title { get; set; }
-        public string Author { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        public int AuthorId { get; set; }
+
+        public Author Author { get; set; }
+
+        [Required]
         public string Publisher { get; set; }
+
         public DateTime PublicatonDate { get; set; }
+
         public int NumberOfPages { get; set; }
     }
 }
